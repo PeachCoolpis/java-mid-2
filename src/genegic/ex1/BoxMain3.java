f6bfd2a2dcd62ef148b93085afd68ce75a797960
PeachCoolpis <rlglxh@naver.com>
2025-10-29 19:34:48 +0900

    제네릭 적용

---
+++ src/genegic/ex1/BoxMain3.java
@@ -0,0 +1,15 @@
+package genegic.ex1;
+
+public class BoxMain3 {
+    public static void main(String[] args) {
+        GenericBox<Integer> integer = new GenericBox<>(1);
+        
+        GenericBox<String> str = new GenericBox<>("hello");
+        System.out.println("integer = " + integer);
+        System.out.println("str = " + str);
+        
+        //타입 추론 : 생성하는 제네릭 타입생략 가능
+        GenericBox<String> str2 = new GenericBox<>("타입추론");
+        
+    }
+}
