ed9d2961c76826051a979490ef449861298685a9
PeachCoolpis <rlglxh@naver.com>
2025-10-29 17:00:40 +0900

    다형성을 통한 중복 해결 시도

---
+++ src/genegic/ex1/BoxMain2.java
@@ -0,0 +1,17 @@
+package genegic.ex1;
+
+public class BoxMain2 {
+    public static void main(String[] args) {
+        ObjectBox objectBox = new ObjectBox();
+        objectBox.setValue(10);
+        Integer integer = (Integer) objectBox.getValue();
+        
+        System.out.println("integer = " + integer);
+        
+        ObjectBox stringBox = new ObjectBox();
+        stringBox.setValue("hello");
+        String value = (String) stringBox.getValue();
+        System.out.println("value = " + value);
+        
+    }
+}
